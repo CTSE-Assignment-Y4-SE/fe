0c0c6608bc93226c3cc023a732177a7d3aff449a
Sapumal Wijekoon <72787452+saputhebeast@users.noreply.github.com>
2025-05-09 21:52:01 +0530

    Fix: url

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosError, AxiosResponse} from "axios";
 import {LoginResponse, UserResponse} from "../types/index.type.ts";
 
-const baseUrl = "http://54.145.3.246/:8080/user/api/v1/auth";
+const baseUrl = "http://54.145.3.246:8080/user/api/v1/auth";
 
 export const login = async (email: string, password: string): Promise<AxiosResponse<LoginResponse>> => {
     try {
